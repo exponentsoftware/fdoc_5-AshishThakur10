138ec6a37ca7a61031971396a8575c7827c2ff3e
AshishThakur10 <sendashishthakur@gmail.com>
2021-07-23 23:17:26 +0530

    fetch added

---
+++ main.js
@@ -0,0 +1,21 @@
+
+fetch = require('cross-fetch');
+
+const API_URL = 'https://restcountries.eu/rest/v2/all'
+
+console.log(API_URL)
+
+// fetch('https://restcountries.eu/rest/v2/all')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+function fetchText() {
+    let response =  fetch('https://restcountries.eu/rest/v2/all')
+    .then(response => response.json())
+    .then(data => console.log(data));
+ 
+}
+
+
+fetchText();
+
