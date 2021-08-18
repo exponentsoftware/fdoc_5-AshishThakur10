0c360b68e4905fcdbe0fc53e93084bd154e2c759
AshishThakur10 <sendashishthakur@gmail.com>
2021-08-18 09:32:31 +0530

    deleted and fixed
    deleted react app

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-
 fetch = require('cross-fetch');
+readline = require('')
 
 const API_URL = 'https://restcountries.eu/rest/v2/all'
 
 console.log(API_URL)
 
-// fetch('https://restcountries.eu/rest/v2/all')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-function fetchText() {
+function fetchText() {   // countries API to fetch data about countries
      fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
     .then(data => console.log(data));
